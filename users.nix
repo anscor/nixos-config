839f69a0524827cb8f555cb5942e8422fe4313ce
anscor <xlyanscor@outlook.com>
2026-09-20 22:40:17 +0800

    在 users.nix 中修复用户配置的结构以正确生成用户属性

--- users.nix
+++ users.nix
@@ -1,9 +1,11 @@
 { lib, pkgs, sUsers, config, ... }: let
   getUserConfig = user: {
-    home = "/home/${user}";
-    extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
-    isNormalUser = true;
-    hashedPasswordFile = config.sops.secrets."anscor_authkey".path;
+    users.users.${user} = {
+      home = "/home/${user}";
+      extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
+      isNormalUser = true;
+      hashedPasswordFile = config.sops.secrets."${toString user}_authkey".path;
+    };
   };
 in
   lib.mergeAttrsList (lib.map getUserConfig sUsers)
