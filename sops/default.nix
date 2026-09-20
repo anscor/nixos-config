c9e102ab5d93dce5087b2573e963b386cade5bed
anscor <xlyanscor@outlook.com>
2026-09-20 23:57:03 +0800

    为 sops 拉取 Go 依赖设置 goproxy.cn 代理

--- sops/default.nix
+++ sops/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
   sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
   sops.age.generateKey = true;
   sops.defaultSopsFile = ./secrets/secrets.yaml;
+
+  # 国内直连 proxy.golang.org 不稳定，改用 goproxy.cn 拉取 Go 依赖
+  sops.package = (inputs.sops-nix.packages.${pkgs.stdenv.hostPlatform.system}.sops-install-secrets).overrideModAttrs (_: prev: {
+    env = (prev.env or { }) // { GOPROXY = "https://goproxy.cn,direct"; };
+  });
 
   sops.secrets."anscor_authkey" = {
     neededForUsers = true;
