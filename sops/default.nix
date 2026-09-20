88477261e2360b40af3083c9114ffd74ff4e45b8
anscor <xlyanscor@outlook.com>
2026-09-21 00:01:10 +0800

    修复 sops GOPROXY 覆盖方式，改用 overrideModAttrs 组合

--- sops/default.nix
+++ sops/default.nix
@@ -1,12 +1,16 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, lib, ... }:
 {
   sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
   sops.age.generateKey = true;
   sops.defaultSopsFile = ./secrets/secrets.yaml;
 
   # 国内直连 proxy.golang.org 不稳定，改用 goproxy.cn 拉取 Go 依赖
-  sops.package = (inputs.sops-nix.packages.${pkgs.stdenv.hostPlatform.system}.sops-install-secrets).overrideModAttrs (_: prev: {
-    env = (prev.env or { }) // { GOPROXY = "https://goproxy.cn,direct"; };
+  sops.package = (inputs.sops-nix.packages.${pkgs.stdenv.hostPlatform.system}.sops-install-secrets).overrideAttrs (final: prev: {
+    passthru = prev.passthru // {
+      overrideModAttrs = lib.composeExtensions prev.passthru.overrideModAttrs (finalMod: prevMod: {
+        env = (prevMod.env or { }) // { GOPROXY = "https://goproxy.cn,direct"; };
+      });
+    };
   });
 
   sops.secrets."anscor_authkey" = {
